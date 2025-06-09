31a5ca6b7e8ed81c1e6dec29cf220fd2e6bcb9cb
Marcus Bernales <marcus.bernales@givecampus.com>
2025-06-09 13:15:55 -0700

    This removes more traces of old plugins

--- src/components/editor/plugins/markdown-plugin.tsx
+++ src/components/editor/plugins/markdown-plugin.tsx
@@ -5,13 +5,11 @@ import {
   remarkMdx,
   remarkMention,
 } from '@udecode/plate-markdown';
-import { SuggestionPlugin } from '@udecode/plate-suggestion/react';
 import remarkGfm from 'remark-gfm';
 import remarkMath from 'remark-math';
 
 export const markdownPlugin = MarkdownPlugin.configure({
   options: {
-    disallowedNodes: [SuggestionPlugin.key],
     remarkPlugins: [remarkMath, remarkGfm, remarkMdx, remarkMention],
   },
 });
